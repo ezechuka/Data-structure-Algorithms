025311b65a3a7172a854ebea5d205044a4a7327a
ezechuka <ezechukwuka9@gmail.com>
2021-09-14 15:09:20 +0100

    implemented queues with ArrayList

---
+++ queues/list/ArrayListQueue.kt
@@ -0,0 +1,20 @@
+import Queue
+
+class ArrayListQueue<T>: Queue<T> {
+    private val list = arrayListOf<T>()
+
+    override val count: Int
+        get() = list.size
+
+    override fun peek() : T? = list.firstOrNull()
+
+    override fun enqueue(element: T): Boolean {
+        list.add(element)
+        return true
+    }
+
+    override fun dequeue(): T? = 
+        if (list.isEmpty()) null else list.removeAt(0)
+
+    override fun toString(): String = list.toString()
+}
---
+++ queues/list/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("----Example of Queue with ArrayList----")
+    val queue = ArrayListQueue<String>().apply {
+        enqueue("Ray")
+        enqueue("Brian")
+        enqueue("Eric")
+    }
+
+    println(queue)
+    queue.dequeue()
+    println(queue)
+    println("Next up: ${queue.peek()}")
+}
